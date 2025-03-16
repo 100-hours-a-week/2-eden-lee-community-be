c79edf4ec84bccaa08a5482d242c594f209d2c58
Yunju07 <juile0827@naver.com>
2025-03-16 15:40:31 +0900

    chore: 스프링 부트 프로젝트 생성

---
+++ src/main/java/com/example/__eden_lee_community_be/Application.java
@@ -0,0 +1,13 @@
+package com.example.__eden_lee_community_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/__eden_lee_community_be/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.__eden_lee_community_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
