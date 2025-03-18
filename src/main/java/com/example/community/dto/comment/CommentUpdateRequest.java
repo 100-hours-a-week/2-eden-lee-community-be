7db015886a5145831b7b4a38346c507c8777ec64
Yunju07 <juile0827@naver.com>
2025-03-18 20:01:10 +0900

    feat: comment 도메인 dto 추가

---
+++ src/main/java/com/example/community/dto/comment/CommentUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.example.community.dto.comment;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+public class CommentUpdateRequest {
+    private Integer userId;
+    private String content;
+}
