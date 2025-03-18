1febaed0c932f92fb27a32966ce0a98f04dc9ee3
Yunju07 <juile0827@naver.com>
2025-03-18 20:19:11 +0900

    feat: user 도메인 dto 추가

---
+++ src/main/java/com/example/community/dto/user/UserUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.example.community.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserUpdateRequest {
+    private String nickname;
+    private String profileImage;
+}
